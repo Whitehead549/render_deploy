d1172166291314faad2599a26f6f479a795ea267
Whitehead549 <finbareric549@gmail.com>
2025-04-06 13:18:03 +0100

    first commit

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ app.post('/api', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-appcls
-.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
